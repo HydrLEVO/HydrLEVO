fbe6e3c9acc36fbb9d2907f7a80254e450c4ee33
HydrLEVO <90807226+HydrLEVO@users.noreply.github.com>
2021-09-16 10:17:16 +1000

    Add files via upload

---
+++ DESKTOP.py
@@ -0,0 +1,34 @@
+import time
+import os
+import socket
+
+
+                                print("DATE: " + time.strftime("%m/%d/%y"))                         print("BATTERY:")print(battery.percent)
+print("""
+
+[g] Google
+[h] Hydra-pad
+[f] file-manager
+[t] terminal
+________________
+[c] close-OS
+""")
+
+select = input("[?]: ")
+     
+if select == 'g':
+    os.startfile("BROWSE-APPS.py")
+    
+if select == 'h':
+    os.startfile("edit.py")
+    
+if select == 'f':    
+  while true:
+     b_login = input(str("please enter password" + 1_n + "to enter your file-manager: ")
+     if b_if login == 1_P
+        print("Starting file-manager")
+        os.startfile("file.py")
+        
+if select == 'c':
+    os.close/quit
+     
